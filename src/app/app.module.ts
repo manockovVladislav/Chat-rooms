12513db3eae1736c5f27052707736c0c928e8bef
Manoczkov Vladislav <manoczkov89@mail.ru>
2018-01-20 13:57:16 +0300

    v 0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,49 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { SharedComponentsComponent } from './shared-components/shared-components.component';
+import { MapComponent } from './shared-components/map/map.component';
+import { HomeComponent } from './containers/home/home.component';
+import { LayoutComponent } from './containers/layout/layout.component';
+import { LoginComponent } from './containers/login/login.component';
+import { ChatComponent } from './containers/home/container-components/chat/chat.component';
+import { ChatCreateComponent } from './containers/home/container-components/chat-create/chat-create.component';
+import { MessageComponent } from './containers/home/container-components/message/message.component';
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import {AngularFireModule} from 'angularfire2';
+
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAJ2zp9kQkPHjY2EYy0zA6_63Xa5QNB3Os',
+  authDomain: 'geochat-b628f.firebaseapp.com',
+  databaseURL: 'https://geochat-b628f.firebaseio.com',
+  projectId: 'geochat-b628f',
+  storageBucket: '',
+  messagingSenderId: '121096908408'
+};
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SharedComponentsComponent,
+    MapComponent,
+    HomeComponent,
+    LayoutComponent,
+    LoginComponent,
+    ChatComponent,
+    ChatCreateComponent,
+    MessageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/containers/home/container-components/chat-create/chat-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-create',
+  templateUrl: './chat-create.component.html',
+  styleUrls: ['./chat-create.component.css']
+})
+export class ChatCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
